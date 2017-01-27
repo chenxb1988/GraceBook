ceb49a560ac573e9229cf8cd3a71605ac2b696ad
chenxb <chenxb@zhuishushenqi.com>
2017-01-27 20:08:18 +0800

    svg

--- app/src/main/java/com/grace/book/activitys/SplashActivity.java
+++ app/src/main/java/com/grace/book/activitys/SplashActivity.java
@@ -5,7 +5,9 @@ import android.widget.ImageView;
 
 import com.grace.book.R;
 import com.grace.book.base.BaseActivity;
+import com.grace.book.beans.SVG;
 import com.grace.book.utils.ActivityUtils;
+import com.grace.book.widget.AnimatedSvgView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,6 +21,9 @@ public class SplashActivity extends BaseActivity {
     @Bind(R.id.iv_splash)
     ImageView mIvSplash;
 
+    @Bind(R.id.animated_svg_view)
+    AnimatedSvgView mSvgView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +36,28 @@ public class SplashActivity extends BaseActivity {
                 ActivityUtils.launchActivity(SplashActivity.this, MainActivity.class, mIvSplash);
                 finish();
             }
-        }, 1000);
+        }, 5000);
+
+        mSvgView.setOnStateChangeListener(new AnimatedSvgView.OnStateChangeListener() {
+
+            @Override
+            public void onStateChange(int state) {
+                if (state == AnimatedSvgView.STATE_FINISHED) {
+                    mSvgView.start();
+                }
+            }
+        });
+        setSvg(SVG.getGraceLogo());
+    }
+
+    private void setSvg(SVG svg) {
+        mSvgView.setGlyphStrings(svg.glyphs);
+        mSvgView.setFillColors(svg.colors);
+        mSvgView.setViewportSize(svg.width, svg.height);
+        mSvgView.setTraceResidueColor(0x32000000);
+        mSvgView.setTraceColors(svg.colors);
+        mSvgView.rebuildGlyphData();
+        mSvgView.start();
     }
 
     @Override
